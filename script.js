0d275f8972f4b9cd1b7b6fede0f5a99d0946a961
augustinthobias2012-ui <augustinthobias2012@gmail.com>
2026-04-18 09:44:26 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//ANIMATION LOGIC
+
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries) {
+entries.forEach(entry => {
+if (!entry.isIntersecting) return;
+entry.target.classList.add('show');
+});
+}, appearOptions);
+
+faders.forEach(fader => {
+appearOnScroll.observe(fader);
+});
